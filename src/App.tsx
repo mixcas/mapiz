50e15b60e2aa6ad77adca563880f5274881d5ec1
Carlos Solares <carlos@videate.io>
2023-12-24 21:19:03 -0600

    remove tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import {  RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
 import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path: "evento/:eventId",
+    element: `evento`,
+  }, {
+    path: "login",
+    element: 'login',
+  }, {
+    path: "archivo",
+    element: `archivo`,
+  // }, {
+  //   path: "/admin/eventos",
+  //   element: 'admin - eventos',
+  // }, {
+  //   path: "/admin/evento/:eventId",
+  //   element: `admin - evento`,
+  // }, {
+  //   path: "/admin/usuarios",
+  //   element: `admin - usuarios`,
+  }, {
+    path: "admin",
+    element: 'admin',
+  }, {
+    path: "/",
+    element: 'home',
+  },
+]);
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <RouterProvider router={router} />
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "evento/:eventId",
-    element: `evento`,
-  }, {
-    path: "login",
-    element: 'login',
-  }, {
-    path: "archivo",
-    element: `archivo`,
-  // }, {
-  //   path: "/admin/eventos",
-  //   element: 'admin - eventos',
-  // }, {
-  //   path: "/admin/evento/:eventId",
-  //   element: `admin - evento`,
-  // }, {
-  //   path: "/admin/usuarios",
-  //   element: `admin - usuarios`,
-  }, {
-    path: "admin",
-    element: 'admin',
-  }, {
-    path: "/",
-    element: <App />
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
