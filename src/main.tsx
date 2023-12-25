51aff14950cf9079ccee451ff5040cc062416e62
Carlos Solares <carlos@videate.io>
2023-12-24 20:38:01 -0600

    set routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,27 @@ import './index.css';
 
 const router = createBrowserRouter([
   {
+    path: "evento/:eventId",
+    element: `evento`,
+  }, {
+    path: "login",
+    element: 'login',
+  }, {
+    path: "archivo",
+    element: `archivo`,
+  // }, {
+  //   path: "/admin/eventos",
+  //   element: 'admin - eventos',
+  // }, {
+  //   path: "/admin/evento/:eventId",
+  //   element: `admin - evento`,
+  // }, {
+  //   path: "/admin/usuarios",
+  //   element: `admin - usuarios`,
+  }, {
+    path: "admin",
+    element: 'admin',
+  }, {
     path: "/",
     element: <App />
   },
